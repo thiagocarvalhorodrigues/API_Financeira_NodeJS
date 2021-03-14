edc0638cbe755c850efea7d11eb7fa7e92b4d765
thiago <tcr.thiago@gmail.com>
2021-03-14 14:27:52 -0300

    Validando CPF existente

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
 
 const express = require('express');
 
+const { v4: uuidv4 } =  require("uuid")
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/courses",(resquest ,response) => {
-    const query = resquest.query
-    console.log(query)
-    return response.json(["Curso 1", "Curso 2","Curso 3"]);
+const customers = [];
+
+app.post("/account",(resquest, response) => {
+    const {cpf, name} = resquest.body;
+    
+    const customerAlreadExists = customers.some( 
+    (customer) => customer.cpf === cpf
+
+);
+
+if (customerAlreadExists) {
+    return response.status(400).json({error: "Custumer alread exists!"});
+
+
+}
+  
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: [],
+
+    });
+
+    return response.status(201).send();
 
 });
 
-app.post("/courses",(resquest,response) => {
-    const body = resquest.body;
-    console.log(body);
-    return response.json(["Curso 1", "Curso 2","Curso 3", "Curso 4"]);
-
-});
-
-app.put("/courses/:id",(resquest,response) => {
-    const  { id } = resquest.params;
-    console.log(id)
-    return response.json(["Curso 6", "Curso 2","Curso 3", "Curso 4"]);
-
-});
-
-app.patch("/courses/:id",(resquest,response) => {
-    return response.json(["Curso 6", "Curso 7","Curso 3", "Curso 4"]);
-});
-
-app.delete("/courses/:id",(resquest,response) =>{
-    return response.json(["Curso 6", "Curso 7", "Curso 4"]);
-
-
-});
 
 
 
