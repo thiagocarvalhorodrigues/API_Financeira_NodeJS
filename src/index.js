1806540ebe3f96bc1404af57547f25b957831b58
thiago <tcr.thiago@gmail.com>
2021-03-17 09:46:00 -0300

    Realizando depósitos

--- src/index.js
+++ src/index.js
@@ -61,4 +61,23 @@ app.get("/statement", verifyExistsAcoountCPF,(request,response) => {
 
 });
 
+app.post("/deposit", verifyExistsAcoountCPF,(request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit",
+
+    };
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+})
+
+
+
 app.listen(3333);
